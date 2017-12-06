34a08627eeda0288e0c2dfb1159b87d1900a0455
Anastacio Gianareas <agianareas@gmail.com>
2017-12-06 14:39:53 -0500

    x

---
+++ src/Container.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Container extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>React</div>;
+  }
+}
